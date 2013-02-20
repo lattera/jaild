ccb74166f67cf23c17eb67fb7d021554c26c63a0
Shawn Webb <swebb@sourcefire.com>
2013-02-20 17:26:41 -0500

    Serialize to json the members of all classes. Add BSD license.

--- classes/Jail.php
+++ classes/Jail.php
@@ -1,5 +1,17 @@
 <?php
 
+/*
+Copyright (c) 2013, Shawn Webb
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+   * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+   * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 class Jail {
     public $name;
     public $path;
@@ -284,6 +296,19 @@ class Jail {
         $serialized["path"] = $this->path;
         $serialized["dataset"] = $this->dataset;
         $serialized["online"] = $this->IsOnline();
+        $serialized["snapshots"] = $this->GetSnapshots();
+
+        $i=0;
+        foreach ($this->network as $n)
+            $serialized["network_" . $i++] = $n->Serialize();
+
+        $i=0;
+        foreach ($this->mounts as $m)
+            $serialized["mount_" . $i++] = $m->Serialize();
+
+        $i=0;
+        foreach ($this->services as $s)
+            $serialized["service_" . $i++] = $s->path;
 
         return $serialized;
     }
--- classes/Mount.php
+++ classes/Mount.php
@@ -1,5 +1,17 @@
 <?php
 
+/*
+Copyright (c) 2013, Shawn Webb
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+   * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+   * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 class Mount {
     public $source;
     public $target;
@@ -27,7 +39,8 @@ class Mount {
         $sth = $db->Query("SELECT * FROM jailadmin_mounts WHRE jail = :name AND target = :target");
         $sth->execute(array(":name" => $jail->name, ":target" => $target));
 
-        return Mount::LoadFromRecord($jail, $sth->fetch(PDO::FETCH_ASSOC));
+        foreach ($sth->fetch() as $record)
+            return Mount::LoadFromRecord($jail, $record);
     }
 
     public static function LoadFromRecord($jail, $record=array()) {
@@ -65,5 +78,14 @@ class Mount {
         $sth = $db->Query("DELETE FROM jailadmin_mounts WHERE jail = :jail AND target = :target");
 
         return  $sth->execute(array(":jail" => $this->jail->name, ":target" => $this->target));
+    }
+
+    public function Serialize() {
+        return array(
+            "source" => $this->source,
+            "target" => $this->target,
+            "driver" => $this->driver,
+            "options" => $this->options,
+        );
     }
 }
